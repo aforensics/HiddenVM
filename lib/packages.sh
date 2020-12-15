230ed7915c0905a72cf0e1b73ff868c093704421
aforensics <59360245+aforensics@users.noreply.github.com>
2020-12-15 10:14:13 +0000

    Fix for a change in Tails 4.14

--- lib/packages.sh
+++ lib/packages.sh
@@ -27,7 +27,7 @@ LAST_PKG_CACHE_PRUNE_DATE_FILE="${CACHE_DIR}/last-pkg-cache-prune"
 PKG_CACHE_PRUNE_FREQ_DAYS=7
 LAST_TAILS_VERSION_FILE="${CACHE_DIR}/last-tails-version"
 LINUX_HEADERS_PKG="linux-headers-$(uname -r)"
-LINUX_HEADERS_RELEASE="sid"
+LINUX_HEADERS_RELEASE="buster-backports" # Changed from "sid" for Tails 4.14
 VBOX_PACKAGE_NAME="virtualbox-6.1"
 
 # SourceList is to replace /etc/apt/sources.list
